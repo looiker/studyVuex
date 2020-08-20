4358f60dbd33172633dcee8ed933e615aac536db
chenboyuan <591221298@qq.com>
2020-08-20 12:15:10 +0800

    vant1

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
+import Button from 'vant/lib/button';
+import 'vant/lib/button/style';
+Vue.use(Button);
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,11 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <button v-on:click="click()">click.me</button>
+    <van-button type="primary" v-on="save()">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
@@ -26,10 +31,13 @@ export default {
    //...mapActions(['saveDbSource']),
     click(){
       //点击一个按钮向store中保存一些数据
-      this.$store.commit('saveCurrDbSource',this.db);
-      this.$store.dispatch('saveDbSource', this.DbSource);
+      // this.$store.commit('saveCurrDbSource',this.db);
+      this.$store.dispatch('state/saveDbSource', this.db);
       //this.saveDbSource;
       alert(this.$store.state.currDbSource);
+    },
+    save(){
+      // this.$store.dispatch('save')
     }
   }
 }
